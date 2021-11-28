b3adb83f5d766350dfa8db1d2a06d1cce6fdab38
DavidKibetRono <david.rono@student.moringaschool.com>
2021-11-28 22:55:04 +0300

    add EventListener to the submit button

--- js/script.js
+++ js/script.js
@@ -94,7 +94,70 @@ $(document).ready(function() {
         $(".slidingImages").css("opacity", "1.0");
     })
 
-    
+    $(document).on("click", "#btn", function() {
+
+        var pizzaSizeValue = parseInt($("#pizzaSize option:selected").val());
+        var pizzaSizeName = $("#pizzaSize option:selected").text();
+
+        if ($("#pizzaSize ")[0].selectedIndex <= 0) {
+
+            alert("You must select the pizza size");
+
+            return false;
+
+        }
+
+        var pizzaCrustValue = parseInt($("#crustTypeInput option:selected").val());
+        var pizzaCrustName = $("#crustTypeInput option:selected").text();
+
+
+        if ($("#crustTypeInput ")[0].selectedIndex <= 0) {
+
+            alert("You must select the type of crust");
+
+            return false;
+
+        }
+
+
+        var toppingsValue = parseInt($("#toppingInput option:selected").val());
+
+
+        var toppingsName = $("#toppingInput option:selected").text();
+
+
+        if ($("#toppingInput ")[0].selectedIndex <= 0) {
+
+            alert("You must select the type of topping");
+
+            return false;
+        }
+
+        var pizzaNumber = parseInt($("#pizzaNumber").val());
+
+        if (!pizzaNumber) {
+            alert("You must select the number of pizza.");
+            return false;
+        } else if (pizzaNumber <= 0) {
+            alert("Number of Pizzas cannot be less 0 or less.")
+        }
+
+        var price = (pizzaSizeValue + pizzaCrustValue + toppingsValue) * pizzaNumber;
+
+        // objects
+        var size = new PizzaName(pizzaSizeName)
+        var crust = new CrustType(pizzaCrustName, pizzaCrustValue)
+        var topping = new Topping(toppingsName)
+
+        $("#checkouts").html("<h3>" + "Pizza Size: " + size.pizzaTotal() + "<br>" + "Crust Type: " + crust.crustName() + "<br>" + "Topping Name: " + topping.toppingName() + "<br>" + "Number of pizzas: " + pizzaNumber + "<br>" + "Total Price = " + "<b>" + "kshs." + price + "</b>" + "<br>" + "<br>" + "<button type='button' class='btn btn-success btn-lg' id='btnDelivery'> Delivery</button>" + " <button type='button' class='btn btn-success btn-lg' id='btnPickUp'> Pick Up </button>" + "</h3>")
+
+        //clear input fields upon submission
+        $("#pizzaSize").get(0).selectedIndex = 0;
+        $("#crustTypeInput").get(0).selectedIndex = 0;
+        $("#toppingInput").get(0).selectedIndex = 0;
+        $("#pizzaNumber").val("")
+
+    })
 
 })
 
